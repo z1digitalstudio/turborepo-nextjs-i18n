c3c0db5eabc42581b5954aaa41f6007c69a4459e
María Simó <holasoymariasimo@gmail.com>
2022-05-17 19:58:20 +0200

    feat(docs): add translations and posts

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,9 +1,23 @@
 import { Button } from "ui";
+import useTranslation from "next-translate/useTranslation";
+import { useRouter } from "next/router";
 
-export default function Docs() {
+export default function Page() {
+  const { t, lang } = useTranslation("common");
+  const router = useRouter();
+
+  const handleLang = (lang: string) =>
+    router.push(router.asPath, router.asPath, { locale: lang });
+
   return (
     <div>
-      <h1>Docs</h1>
+      <h1>{t("title")}</h1>
+      <p>{lang}</p>
+      <a href={`http://localhost:3000/${lang}`}>Back to home</a>
+      <br></br>
+      <br></br>
+      <button onClick={() => handleLang("en")}>en</button>
+      <button onClick={() => handleLang("es")}>es</button>
       <Button />
     </div>
   );
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,13 +1,23 @@
 import { Button } from "ui";
 import useTranslation from "next-translate/useTranslation";
+import { useRouter } from "next/router";
 
 export default function Page() {
   const { t, lang } = useTranslation("common");
+  const router = useRouter();
+
+  const handleLang = (lang: string) =>
+    router.push(router.asPath, router.asPath, { locale: lang });
 
   return (
     <div>
       <h1>{t("title")}</h1>
       <p>{lang}</p>
+      <a href={`http://localhost:3001/${lang}`}>Go to docs</a>
+      <br></br>
+      <br></br>
+      <button onClick={() => handleLang("en")}>en</button>
+      <button onClick={() => handleLang("es")}>es</button>
       <Button />
     </div>
   );
